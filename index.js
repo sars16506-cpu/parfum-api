06bac553c33cfe7e8d7ff93f253bfda5d45218e4
X KING <sars16506@gmail.com>
2026-02-27 09:56:54 +0500

    bott

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ console.log({
 });
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,x-bot-secret");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
